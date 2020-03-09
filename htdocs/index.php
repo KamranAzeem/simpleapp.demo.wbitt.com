0ac42bf3b1f238b629cc97a9316d480a45cd9c9a
Kamran Azeem <kaz@praqma.net>
2020-03-09 02:30:51 +0100

    added HR to index.* files

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,5 +5,6 @@ echo "<a href=index.html>index.html</a>" . PHP_EOL . "<br>";
 echo "<a href=phpinfo.php>phpinfo.php</a>" . PHP_EOL . "<br>";
 echo "<a href=createfile.php>createfile.php</a>"  . PHP_EOL . "<br>";
 echo "<a href=dbconnection.php>dbconnection.php</a>"  . PHP_EOL . "<br>";
+echo "<hr>";
 ?> 
 
